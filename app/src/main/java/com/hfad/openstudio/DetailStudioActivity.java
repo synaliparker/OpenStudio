0666c9692fdba35bc345cccf0e028fa7ebd62153
reginayiwang <reginawang@live.com>
2017-05-01 12:57:09 -0700

    Added detail page and more UI changes

--- app/src/main/java/com/hfad/openstudio/DetailStudioActivity.java
+++ app/src/main/java/com/hfad/openstudio/DetailStudioActivity.java
@@ -1,13 +1,271 @@
 package com.hfad.openstudio;
 
 import android.app.Activity;
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class DetailStudioActivity extends Activity {
+    private static final String TAG = RegisterActivity.class.getSimpleName();
+    private ProgressDialog pDialog;
+    private static final String STUDIO_DETAIL_PATH = "get_studio_details.php";
+    private static final String STUDIO_DETAIL_URL = "http://open-studio.herokuapp.com/get_studio_details.php?id=";
+    private TextView txtName;
+    private TextView txtType;
+    private TextView txtOwner;
+    private TextView txtMorePhotos;
+    private TextView txtAddress;
+    private TextView txtEmail;
+    private TextView txtAvailability;
+    private TextView txtAccessibility;
+    private TextView txtDescription;
+
+    private static final String TAG_SUCCESS = "success";
+    private static final String TAG_STUDIOS = "studios";
+    private static final String TAG_STUDIO = "studio";
+    private static final String TAG_NAME = "name";
+    private static final String TAG_ID = "id";
+    private static final String TAG_TYPE = "type";
+    private static final String TAG_OWNER = "owner";
+    private static final String TAG_ADDRESS = "address";
+    private static final String TAG_CONTACT = "contact_info";
+    private static final String TAG_AVAIL = "availability";
+    private static final String TAG_ACCESS = "accessibility";
+    private static final String TAG_DESCRIPTION = "description";
+
+    String studioId;
+    HttpRequestHandler httpRequestHandler = new HttpRequestHandler();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail_studio);
+
+        Intent intent = getIntent();
+        studioId = intent.getStringExtra(TAG_ID);
+        Log.d("ID", studioId);
+        pDialog = new ProgressDialog(this);
+        pDialog.setCancelable(false);
+        getStudioDetails(studioId.toString());
+//        new GetStudioDetails().execute();
+        txtMorePhotos = (TextView) findViewById(R.id.more_photos);
+        txtMorePhotos.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(DetailStudioActivity.this, ImageDetailActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    private void getStudioDetails(final String studioId) {
+        // Tag used to cancel the request
+        String tag_string_req = "req_details";
+
+        pDialog.setMessage("Getting studio details ...");
+        showDialog();
+
+        StringRequest strReq = new StringRequest(Request.Method.GET,
+                STUDIO_DETAIL_URL + studioId, new Response.Listener<String>() {
+
+            @Override
+            public void onResponse(String response) {
+                Log.d(TAG, "Response: " + response.toString());
+                hideDialog();
+
+                try {
+                    JSONObject jObj = new JSONObject(response);
+                    boolean error = jObj.getBoolean("error");
+
+                    // Check for error node in json
+                    if (!error) {
+                        JSONArray studioArray = jObj.getJSONArray(TAG_STUDIO);
+                        JSONObject studio = studioArray.getJSONObject(0);
+                        txtName = (TextView) findViewById(R.id.studio_name);
+                        txtType = (TextView) findViewById(R.id.studio_type);
+                        txtOwner = (TextView) findViewById(R.id.studio_owner);
+                        txtAddress = (TextView) findViewById(R.id.studio_address);
+                        txtEmail = (TextView) findViewById(R.id.studio_contact);
+                        txtAvailability = (TextView) findViewById(R.id.studio_availability);
+                        txtAccessibility = (TextView) findViewById(R.id.studio_accessibility);
+                        txtDescription = (TextView) findViewById(R.id.studio_description);
+
+                        txtName.setText(studio.getString(TAG_NAME));
+                        txtType.setText(studio.getString(TAG_TYPE));
+                        txtOwner.setText(studio.getString(TAG_OWNER));
+                        txtAddress.setText("Address: " + studio.getString(TAG_ADDRESS));
+                        txtEmail.setText("Email: " + studio.getString(TAG_CONTACT));
+                        txtAvailability.setText("Availability: " + studio.getString(TAG_AVAIL));
+                        txtAccessibility.setText("Accessibility: " + studio.getString(TAG_ACCESS));
+                        txtDescription.setText("Description: " + studio.getString(TAG_DESCRIPTION));
+                    } else {
+                        // Error in login. Get the error message
+                        String errorMsg = jObj.getString("error_msg");
+                        Toast.makeText(getApplicationContext(),
+                                errorMsg, Toast.LENGTH_LONG).show();
+                    }
+                } catch (JSONException e) {
+                    // JSON error
+                    e.printStackTrace();
+                    Toast.makeText(getApplicationContext(), "Json error: " + e.getMessage(), Toast.LENGTH_LONG).show();
+                }
+
+            }
+        }, new Response.ErrorListener() {
+
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.e(TAG, "Error: " + error.getMessage());
+                Toast.makeText(getApplicationContext(),
+                        error.getMessage(), Toast.LENGTH_LONG).show();
+                hideDialog();
+            }
+        }) {
+
+//            @Override
+//            protected Map<String, String> getParams() {
+//                // Posting parameters to login url
+//                Map<String, String> params = new HashMap<String, String>();
+//                params.put("id", studioId);
+//
+//                return params;
+//            }
+
+        };
+
+        // Adding request to request queue
+        Log.d("Status", strReq.toString());
+        AppController.getInstance().addToRequestQueue(strReq, tag_string_req);
+    }
+
+    private void showDialog() {
+        if (!pDialog.isShowing())
+            pDialog.show();
+    }
+
+    private void hideDialog() {
+        if (pDialog.isShowing())
+            pDialog.dismiss();
     }
 }
+
+//    /**
+//     * Background Async Task to Get complete product details
+//     * */
+//    class GetStudioDetails extends AsyncTask<String, String, String> {
+//
+//        /**
+//         * Before starting background thread Show Progress Dialog
+//         * */
+//        @Override
+//        protected void onPreExecute() {
+//            super.onPreExecute();
+//            pDialog = new ProgressDialog(DetailStudioActivity.this);
+//            pDialog.setMessage("Loading studio details. Please wait...");
+//            pDialog.setIndeterminate(false);
+//            pDialog.setCancelable(true);
+//            pDialog.show();
+//        }
+//
+//        /**
+//         * Getting product details in background thread
+//         * */
+//        protected String doInBackground(String... params) {
+//
+//            // updating UI from Background Thread
+//            runOnUiThread(new Runnable() {
+//                public void run() {
+//                    // Check for success tag
+//                    int success;
+//                    try {
+//                        // Building Parameters
+//                        HashMap<String, String> hashMap = new HashMap<>();
+//                        hashMap.put(TAG_ID, studio_id.toString());
+//
+//                        // getting product details by making HTTP request
+//                        // Note that product details url will use GET request
+//                        JSONObject json = httpRequestHandler.makeHttpRequest(
+//                                STUDIO_DETAIL_PATH, "GET", hashMap);
+//                        Log.d("JSON", json.toString());
+//
+//                        // json success tag
+//                        try {
+//                            success = json.getInt(TAG_SUCCESS);
+//                            Log.d("success", String.valueOf(success));
+//                        } catch (JSONException e) {
+//                            Log.d("Error", "JSON exception");
+//                            success = 3;
+//                        }
+//                        if (success == 1) {
+//                            // successfully received product details
+//                            JSONArray studioDetails = json
+//                                    .getJSONArray(TAG_STUDIOS); // JSON Array
+//                            JSONObject studio = studioDetails.getJSONObject(0);
+//                            for (int i = 0; i < studioDetails.length(); i++) {
+//                                Log.d("id tag", studioDetails.getJSONObject(i).getString(TAG_ID));
+//                                if (studioDetails.getJSONObject(i).getString(TAG_ID).equals(studio_id)) {
+//                                    studio = studioDetails.getJSONObject(i);
+//                                }
+//                            }
+//
+//                            // product with this pid found
+//                            // Edit Text
+//                            txtName = (TextView) findViewById(R.id.studio_name);
+//                            txtType = (TextView) findViewById(R.id.studio_type);
+//                            txtOwner = (TextView) findViewById(R.id.studio_owner);
+//                            txtAddress = (TextView) findViewById(R.id.studio_address);
+//                            txtEmail = (TextView) findViewById(R.id.studio_contact);
+//                            txtAvailability = (TextView) findViewById(R.id.studio_availability);
+//                            txtAccessibility = (TextView) findViewById(R.id.studio_accessibility);
+//                            txtDescription = (TextView) findViewById(R.id.studio_description);
+//
+//                            // display product data in EditText
+//                            txtName.setText(studio.getString(TAG_NAME));
+//                            txtType.setText(studio.getString(TAG_TYPE));
+//                            txtOwner.setText(studio.getString(TAG_OWNER));
+//                            txtAddress.setText("Address: " + studio.getString(TAG_ADDRESS));
+//                            txtEmail.setText("Email: " + studio.getString(TAG_CONTACT));
+//                            txtAvailability.setText("Availability: " + studio.getString(TAG_AVAIL));
+//                            txtAccessibility.setText("Accessibility: " + studio.getString(TAG_ACCESS));
+//                            txtDescription.setText("Description: " + studio.getString(TAG_DESCRIPTION));
+//
+//                        }else{
+//                            // product with pid not found
+//                        }
+//                    } catch (JSONException e) {
+//                        e.printStackTrace();
+//                    }
+//                }
+//            });
+//
+//            return null;
+//        }
+//
+//        /**
+//         * After completing background task Dismiss the progress dialog
+//         * **/
+//        protected void onPostExecute(String file_url) {
+//            // dismiss the dialog once got all details
+//            pDialog.dismiss();
+//        }
+//    }
+
---
+++ app/src/main/java/com/hfad/openstudio/ImageDetailActivity.java
@@ -0,0 +1,69 @@
+package com.hfad.openstudio;
+
+import android.app.Activity;
+import android.content.Context;
+import android.os.Bundle;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+
+public class ImageDetailActivity extends Activity {
+    ViewPager mViewPager;
+    int[] mResources = {
+            R.drawable.image_1,
+            R.drawable.image_2,
+            R.drawable.image_3,
+            R.drawable.image_4,
+    };
+
+    /**
+     * Credit: http://stackoverflow.com/questions/29991780/how-could-i-develop-an-image-carousel-on-android
+     */
+    public class ImagePagerAdapter extends PagerAdapter {
+        private Context mContext;
+        private LayoutInflater mLayoutInflater;
+
+        public ImagePagerAdapter(Context context) {
+            mContext = context;
+            mLayoutInflater = (LayoutInflater) mContext.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        }
+
+        @Override
+        public int getCount() {
+            return mResources.length;
+        }
+
+        @Override
+        public boolean isViewFromObject(View view, Object object) {
+            return view == object;
+        }
+
+        @Override
+        public Object instantiateItem(ViewGroup container, int position) {
+            View itemView = mLayoutInflater.inflate(R.layout.viewpager_item, container, false);
+
+            ImageView imageView = (ImageView) itemView.findViewById(R.id.imageView);
+            imageView.setImageResource(mResources[position]);
+            container.addView(itemView);
+            return itemView;
+        }
+
+        @Override
+        public void destroyItem(ViewGroup container, int position, Object object) {
+            container.removeView((LinearLayout) object);
+        }
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_image_detail);
+        ImagePagerAdapter imagePagerAdapter = new ImagePagerAdapter(this);
+        mViewPager = (ViewPager) findViewById(R.id.pager);
+        mViewPager.setAdapter(imagePagerAdapter);
+    }
+}
