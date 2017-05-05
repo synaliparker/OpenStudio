fc917affbce1205edf638de9bac56f1fa17d0b02
Synali Parker <synaparker@gmail.com>
2017-05-04 23:23:13 -0700

    Added Bottom Nav View. (App Crash Errors)

--- app/src/main/java/edu/mills/openstudio/MainActivity.java
+++ app/src/main/java/edu/mills/openstudio/MainActivity.java
@@ -3,19 +3,52 @@ package edu.mills.openstudio;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-    private BottomNavigationView bottomBar;
+
+    private TextView textHome;
+    private TextView textSearch;
+    private TextView textAccount;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-       // bottomBar = BottomNavigationView.bind(this,R.layout.activity_main, savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+//        textHome = (TextView) findViewById(R.id.text_home);
+//        textSearch = (TextView) findViewById(R.id.text_search_studios);
+//        textAccount = (TextView) findViewById(R.id.text_account);
+
+        BottomNavigationView bottomNavigationView = (BottomNavigationView)
+                findViewById(R.id.bottom_navigation);
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(
+                new BottomNavigationView.OnNavigationItemSelectedListener(){
+                    @Override
+                    public boolean onNavigationItemSelected(@NonNull MenuItem item){
+                        switch (item.getItemId()){
+                            case R.id.home_item:
+                                Toast.makeText(MainActivity.this, "Button clicked", Toast.LENGTH_SHORT).show();
+                                break;
+                            case R.id.search_studios_item:
+                                Toast.makeText(MainActivity.this, "Button clicked", Toast.LENGTH_SHORT).show();
+                                break;
+                            case R.id.account_item:
+                                Toast.makeText(MainActivity.this, "Button clicked", Toast.LENGTH_SHORT).show();
+                                break;
+                        }
+                        return false;
+                    }
+
+                });
     }
 
     @Override
@@ -32,7 +65,7 @@ public class MainActivity extends AppCompatActivity {
                 Intent intent = new Intent(this, AddStudioActivity.class);
                 startActivity(intent);
                 return true;
-                // Code to run when the about item is clicked
+            // Code to run when the about item is clicked
             case R.id.action_settings:
                 //Code to run when the settings item is clicked
                 return true;
