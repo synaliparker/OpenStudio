b9b2171089700ac53bc1475b6f647d8e8f842820
reginayiwang <reginawang@live.com>
2017-05-07 21:21:56 -0700

    Got rid of hardcoded strings and dimensions in XML files.

--- app/src/main/java/edu/mills/openstudio/AboutActivity.java
+++ app/src/main/java/edu/mills/openstudio/AboutActivity.java
@@ -3,10 +3,6 @@ package edu.mills.openstudio;
 import android.app.Activity;
 import android.os.Bundle;
 
-/**
- * Created by Evetallula on 5/4/2017.
- */
-
 public class AboutActivity extends Activity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
